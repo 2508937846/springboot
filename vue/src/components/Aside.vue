92b5223d5c0e28ddbaa02480adf7b1374995231e
WANGTINGXIAN <2508937846@qq.com>
2021-11-15 12:47:48 +0800

    Initial commit

---
+++ vue/src/components/Aside.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <el-menu
+        style="width: 200px; min-height: calc(100vh - 50px)"
+        :default-active="$route.path"
+        router
+    >
+      <el-menu-item index="/home">
+        <i class="el-icon-house"></i>  主页
+      </el-menu-item>
+      <el-menu-item index="/category">
+        <i class="el-icon-menu"></i> 分类管理
+      </el-menu-item>
+      <el-menu-item index="/book">
+        <i class="el-icon-files"></i> 书籍管理
+      </el-menu-item>
+      <el-menu-item index="/order">
+        <i class="el-icon-s-order"></i> 我的订单
+      </el-menu-item>
+      <el-menu-item index="/news">
+        <i class="el-icon-news"></i> 新闻管理
+      </el-menu-item>
+      <el-menu-item index="/map"><i class="el-icon-map-location"></i> 百度地图</el-menu-item>
+      <el-menu-item index="/im">
+        <i class="el-icon-chat-round"></i> 聊天室
+      </el-menu-item>
+      <el-menu-item index="/message">
+        <i class="el-icon-message"></i> 聊天室在线留言
+      </el-menu-item>
+      <el-submenu index="1" v-if="user.role === 1">
+        <template #title>
+          <i class="el-icon-s-tools"></i> 系统管理
+        </template>
+        <el-menu-item index="/user">
+          <i class="el-icon-user"></i> 用户管理
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="/donate">
+        <i class="el-icon-coffee"></i> 读书分享
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+import request from "@/utils/request";
+
+export default {
+  name: "Aside",
+  data() {
+    return {
+      user: {}
+    }
+  },
+  created() {
+    let userStr = sessionStorage.getItem("user") || "{}"
+    this.user = JSON.parse(userStr)
+
+    // 请求服务端，确认当前登录用户的 合法信息
+    request.get("/user/" + this.user.id).then(res => {
+      if (res.code === '0') {
+        this.user = res.data
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/src/views/Donate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="padding: 10px">
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card>
+          <div style="padding: 10px 0; font-size: 18px">王廷宪</div>
+          <el-image style="width: 240px" :src="wx" :preview-src-list="[wx]"></el-image>
+          <el-image style="width: 240px; margin-left: 100px" :src="alipay" :preview-src-list="[alipay]"></el-image>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Donate",
+  data() {
+    return {
+      wx: "https://pica.zhimg.com/80/v2-40d70c1000e302404df8723ec13bceac_720w.jpg?source=1940ef5c",
+      alipay: 'https://pic2.zhimg.com/80/v2-d43ac1ce134ee24fa0d89e55bd30ad7b_720w.jpg?source=1940ef5c'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
